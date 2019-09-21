e195a92630df2339cd8d7c5f4e891a5a7131db57
gb <13711279334@163.com>
2019-09-21 21:14:45 +0800

    logout

--- app/src/main/java/com/ixuea/courses/kanmeitu/MainActivity.java
+++ app/src/main/java/com/ixuea/courses/kanmeitu/MainActivity.java
@@ -2,13 +2,30 @@ package com.ixuea.courses.kanmeitu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.ixuea.courses.kanmeitu.util.SharePreferencesUtil;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharePreferencesUtil sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sp = SharePreferencesUtil.getInstance((getApplicationContext()));
+    }
+
+    public void onlogoutCLick(View view) {
+         sp.setLogin(false);
+
+        Intent intent = new Intent(this, loginActivity.class);
+        startActivity(intent);
+
+        finish();
     }
 }
